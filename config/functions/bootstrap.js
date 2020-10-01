121ffdababf91324fbcaed8f1058c8e461db16f1
ToraNova <chia_jason96@live.com>
2020-10-02 05:53:37 +0800

    update

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -43,24 +43,21 @@ module.exports = async () => {
 		})
 
 		//listen for map initial data request
-		socket.on('focus/init', (msg) => {
+		socket.on('focus/init', async (msg) => {
 			if(msg){
 				msg = JSON.parse(msg)
 			}else{
 				msg = {count: 10}
 			}
 
-			strapi.query('alert').find({
+			const res = await strapi.query('alert').find({
 				Reason_null:true,
 				_limit: msg.count || 10
-			}).then( (res) => {
-				socket.emit('focus/alert/data', JSON.stringify(res));
-			}).catch( err => {
-				strapi.log.error(`focus/init ${err.message}`)
 			});
+			socket.emit('focus/alert/data', JSON.stringify(res));
 		})
 
-		socket.on('focus/live', (msg) => {
+		socket.on('focus/live', async (msg) => {
 			msg = JSON.parse(msg)
 
 			if( msg.command == 'stop' ){
@@ -74,40 +71,32 @@ module.exports = async () => {
 				return
 			}
 
-			strapi.query('fence-segment')
-			.model.query(qb => {
-				qb.where('id', msg.fseg || 0);
-			}).fetch({require:true}).then( (sr) => {
-				const s = sr.toJSON();
-				if(s.ip_camera === null){
-					// no cameras
-				}else{
-					strapi.query('ip-camera')
-					.model.query(qb => {
-						qb.where('id', s.ip_camera.id);
-					}).fetch().then( (cr) => {
-						const e = cr.toJSON();
-						//craft a MQTT packet
-						var nopro = e.Domain.split(/:(.+)/)[1]
-						var livecom = {
-							fsegid: msg.fseg,
-							video: msg.vnum,
-							mode: msg.command,
-							url:`rtsp:${nopro}${e.ip_camera_model.StreamPath}`,
-						}
-						if(e.UseDefaultLogin){
-							livecom.user = e.ip_camera_model.GlobalUsername
-							livecom.pass = e.ip_camera_model.GlobalPassword
-						}else{
-							livecom.user = e.Username
-							livecom.pass = e.Password
-						}
-						strapi.ssmqtt.publish('camlive', JSON.stringify(livecom))
-					});
+			const seg = await strapi.query('fence-segment').findOne({ id: msg.fseg || 0});
+			if(seg === null ){
+				strapi.log.warn(`focus/live segment id ${msg.fset} doesn\'t exist`);
+			}
+
+			if(seg.ip_camera === null){
+				//no cameras
+			}else{
+				const e = seg.ip_camera
+				//craft a MQTT packet
+				var nopro = e.Domain.split(/:(.+)/)[1]
+				var livecom = {
+					fsegid: msg.fseg,
+					video: msg.vnum,
+					mode: msg.command,
+					url:`rtsp:${nopro}${e.ip_camera_model.StreamPath}`,
 				}
-			}).catch( err => {
-				strapi.log.error(`focus/live ${err.message}`)
-			});
+				if(e.UseDefaultLogin){
+					livecom.user = e.ip_camera_model.GlobalUsername
+					livecom.pass = e.ip_camera_model.GlobalPassword
+				}else{
+					livecom.user = e.Username
+					livecom.pass = e.Password
+				}
+				strapi.ssmqtt.publish('camlive', JSON.stringify(livecom))
+			}
 		})
 
 		//forward this message to map/alert/highlight
--- hooks/ssmqtt/reply.js
+++ hooks/ssmqtt/reply.js
@@ -3,21 +3,11 @@
 module.exports = (topic, message, strapi) => {
 	//strapi.log.info(`Reply handler`)
 
-	strapi.models['fence-host'].forge({
-		id: message.id
-	}).fetch({
-
-	}).then( host => {
-		if( host == null ) {}
-		else {
-			var now = new Date()
-			var dstr = now.toISOString()
-			host.set({
-				'RepliedPing':true,
-				'LastHeard': dstr
-			}).save()
-		}
-	}).catch( err => {
-		strapi.log.error('SSMQTT PingReply',err.message)
-	})
+	var now = new Date()
+	var dstr = now.toISOString()
+	strapi.query('fence-host').update({id:message.id
+	},{
+		RepliedPing:true,
+		LastHeard: dstr
+	});
 }
